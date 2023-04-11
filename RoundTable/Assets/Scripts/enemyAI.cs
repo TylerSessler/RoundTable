2f3b40ab80ed5870444cbf89fe31feddb01fb090
JosephWilhour <jhwilhour@student.fullsail.edu>
2023-04-10 16:49:32 -0800

    Roaming implemented (partial)
    Roaming works, though seems to randomly stop & only works on the boss for unknown reasons.

--- RoundTable/Assets/Scripts/enemyAI.cs
+++ RoundTable/Assets/Scripts/enemyAI.cs
@@ -15,6 +15,8 @@ public class enemyAI : MonoBehaviour, IDamage
     [Range(1, 10)][SerializeField] int HP;
     [SerializeField] int playerFaceSpeed;
     [SerializeField] int sightAngle;
+    [SerializeField] int roamRange;
+    [SerializeField] float roamCooldown;
 
     [Header("----- Enemy Gun -----")]
     [Range(1, 10)][SerializeField] int shootDamage;
@@ -31,6 +33,7 @@ public class enemyAI : MonoBehaviour, IDamage
     float angleToPlayer;
     bool isShooting;
     float stoppingDistOrig;
+    bool isRoaming;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +51,11 @@ public class enemyAI : MonoBehaviour, IDamage
             {
 
             }
+        }
+        // Make sure enemy isn't pathing currently. Both for agro & for current roaming if it is past the cooldown timer while still moving
+        if (!strippedVision() && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            StartCoroutine(roam());
         }
     }
 
@@ -77,6 +85,23 @@ public class enemyAI : MonoBehaviour, IDamage
         return false;
     }
 
+    bool strippedVision()
+    {
+        playerDir = (gameManager.instance.player.transform.position - headPos.position);
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= sightAngle)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
     IEnumerator shoot()
     {
         float vertOffset = Random.Range(vertSpread * -1, vertSpread);
@@ -87,6 +112,23 @@ public class enemyAI : MonoBehaviour, IDamage
         bulletClone.GetComponent<Rigidbody>().velocity = new Vector3(playerDir.x + horiOffset, playerDir.y + vertOffset, playerDir.z) * bulletSpeed;
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
+    }
+
+    IEnumerator roam()
+    {
+        Debug.Log("roam called");
+        isRoaming = true;
+        NavMeshHit hit;
+        Vector3 roamDestination = headPos.position + Random.insideUnitSphere * roamRange;
+        if (NavMesh.SamplePosition(roamDestination, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            Debug.Log("Set position");
+            agent.stoppingDistance = 0;
+            agent.SetDestination(hit.position);
+        }
+        yield return new WaitForSeconds(roamCooldown);
+        isRoaming = false;
+        agent.stoppingDistance = stoppingDistOrig;
     }
 
     void OnTriggerEnter(Collider other)
