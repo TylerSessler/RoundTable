fbb6e46136f128e65e8a1c315aecdcfcffdd122d
john109695 <64110752+JohnAdams2@users.noreply.github.com>
2023-05-08 15:16:18 -0400

    Added scene manager script
    Can now implement loading scenes.

---
+++ RoundTable/Assets/Scripts/scenesManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenesManager : MonoBehaviour
+{
+    public static scenesManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public enum Scene
+    {
+        MainMenu,
+        TutorialLevel,
+        ShopLevel,
+        Level01,
+        Level02,
+        Level03,
+    }
+
+    public void LoadScene(Scene s)
+    {
+        SceneManager.LoadScene(s.ToString());
+    }
+    public void LoadNewGame()
+    {
+        SceneManager.LoadScene(Scene.TutorialLevel.ToString());
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(Scene.MainMenu.ToString());
+    }
+}
